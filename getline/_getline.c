3b3dc9cd9903c0893f50f239af9e5a6e570e8953
Natdog225 <Nathaniel.wilson@atlasschool.com>
2025-01-13 14:01:28 -0600

    Added a check so it will end even if its not on a newline

--- getline/_getline.c
+++ getline/_getline.c
@@ -3,32 +3,19 @@
 #include <unistd.h>
 #include <stdio.h>
 
-#define MAX_LINE_LENGTH 25
-
-
-static char buffer[MAX_FDS][READ_SIZE + 1] = {{0}};
-static int read_bytes[MAX_FDS] = {0};
-static int offset[MAX_FDS] = {0};
-static int total_lines = 0;
-
 char *_getline(const int fd)
 {
+	static char buffer[MAX_FDS][READ_SIZE + 1] = {{0}};
+	static int read_bytes[MAX_FDS] = {0};
+	static int offset[MAX_FDS] = {0};
 	char *line = NULL;
 	int line_len = 0;
-	int i;
 
-	if (fd == -1)
-	{
-		for (i = 0; i < MAX_FDS; i++)
-		{
-			read_bytes[i] = 0;
-			offset[i] = 0;
-			buffer[i][0] = '\0';
-		}
-		return (NULL);
-	}
 	if (fd < 0 || fd >= MAX_FDS)
-		return (NULL);
+	{
+		fprintf(stderr, "Invalid file descriptor\n");
+		return NULL;
+	}
 
 	while (1)
 	{
@@ -36,52 +23,50 @@ char *_getline(const int fd)
 		{
 			offset[fd] = 0;
 			read_bytes[fd] = read(fd, buffer[fd], READ_SIZE);
-			if (read_bytes[fd] == 0)
+			if (read_bytes[fd] <= 0)
 			{
 				if (line_len == 0)
-					return (NULL);
+				{
+					return NULL;
+				}
 				break;
 			}
-			if (read_bytes[fd] == -1)
-				return (NULL);
 			buffer[fd][read_bytes[fd]] = '\0';
 		}
-		for (i = offset[fd]; i < read_bytes[fd]; i++)
+
+		for (int i = offset[fd]; i < read_bytes[fd]; i++)
 		{
-			if (line_len >= MAX_LINE_LENGTH)
-				break;
-
-			line = realloc(line, line_len + 2);
-			if (!line)
-				return (NULL);
-
-			line[line_len++] = buffer[fd][i];
-
 			if (buffer[fd][i] == '\n')
 			{
+				line = realloc(line, line_len + 1);
+				if (!line)
+				{
+					perror("Memory allocation failed");
+					return NULL;
+				}
+				strncpy(line, buffer[fd] + offset[fd], line_len);
 				line[line_len] = '\0';
 				offset[fd] = i + 1;
-				total_lines++;
-				printf("Total: %d lines\n", total_lines);
-				return (line);
+				return line;
 			}
+			line_len++;
 		}
-		offset[fd] = i;
+		offset[fd] = read_bytes[fd];
 	}
 
-	if (line_len > 0 && line[line_len - 1] == '\n')
+	/* EOF without newline check */
+	if (line_len > 0)
 	{
-		line[line_len - 1] = '\0';
-		line_len--;
+		line = realloc(line, line_len + 1);
+		if (!line)
+		{
+			perror("Memory allocation failed");
+			return NULL;
+		}
+		strncpy(line, buffer[fd] + offset[fd], line_len);
+		line[line_len] = '\0';
+		return line;
 	}
 
-	line = realloc(line, line_len + 1);
-	if (!line)
-		return (NULL);
-	line[line_len] = '\0';
-
-	total_lines++;
-	printf("Total: %d lines\n", total_lines);
-
-	return (line);
+	return NULL;
 }
