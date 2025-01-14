30b87868c1e5f783bab1306060f049807b06c9a9
Natdog225 <Nathaniel.wilson@atlasschool.com>
2025-01-14 11:06:02 -0600

    Tried to simplify it down a bit. Which I dont think I succeeded but it works better!

--- getline/_getline.c
+++ getline/_getline.c
@@ -9,93 +9,80 @@
 char *_getline(const int fd)
 {
 	static char buffer[MAX_FDS][READ_SIZE + 1] = {{0}};
-	static int read_bytes[MAX_FDS] = {0};
+	static int bytes_read[MAX_FDS] = {0};
 	static int offset[MAX_FDS] = {0};
 	static int line_count = 0;
 	char *line = NULL;
 	int line_len = 0;
+	int i, newline_pos;
 
 	if (fd == -1)
 	{
-		for (int i = 0; i < MAX_FDS; i++)
+		for (i = 0; i < MAX_FDS; i++)
 		{
-			read_bytes[i] = 0;
+			bytes_read[i] = 0;
 			offset[i] = 0;
-			buffer[i][0] = '\0';
 		}
 		line_count = 0;
-		return (NULL);
-	}
-
-	if (fd < 0 || fd >= MAX_FDS)
-	{
-		fprintf(stderr, "Invalid file descriptor\n");
 		return NULL;
 	}
 
-	if (line_count >= MAX_LINES)
+	if (fd < 0 || fd >= MAX_FDS || line_count >= MAX_LINES)
 	{
 		return NULL;
 	}
 
 	while (1)
 	{
-		if (offset[fd] >= read_bytes[fd])
+		if (offset[fd] == bytes_read[fd])
 		{
+			bytes_read[fd] = read(fd, buffer[fd], READ_SIZE);
 			offset[fd] = 0;
-			read_bytes[fd] = read(fd, buffer[fd], READ_SIZE);
-			if (read_bytes[fd] <= 0)
+			if (bytes_read[fd] <= 0)
 			{
 				if (line_len == 0)
-				{
 					return NULL;
-				}
 				break;
 			}
-			buffer[fd][read_bytes[fd]] = '\0';
 		}
 
-		for (int i = offset[fd]; i < read_bytes[fd]; i++)
+		newline_pos = -1;
+		for (i = offset[fd]; i < bytes_read[fd]; i++)
 		{
-			line_len++;
 			if (buffer[fd][i] == '\n')
 			{
-				line = realloc(line, line_len + 1);
-				if (!line)
-				{
-					perror("Memory allocation failed");
-					return NULL;
-				}
-				memcpy(line + line_len - (i - offset[fd] + 1), buffer[fd] + offset[fd], i - offset[fd] + 1);
-				line[line_len] = '\0';
-				offset[fd] = i + 1;
-				line_count++;
-				return line;
+				newline_pos = i;
+				break;
 			}
 		}
 
-		line = realloc(line, line_len + 1);
-		if (!line)
+		if (newline_pos != -1)
 		{
-			perror("Memory allocation failed");
-			return NULL;
+			line = realloc(line, line_len + (newline_pos - offset[fd]) + 2);
+			if (!line)
+				return NULL;
+			memcpy(line + line_len, buffer[fd] + offset[fd], newline_pos - offset[fd] + 1);
+			line_len += (newline_pos - offset[fd] + 1);
+			line[line_len] = '\0';
+			offset[fd] = newline_pos + 1;
+			line_count++;
+			return line;
 		}
-		memcpy(line + line_len - (read_bytes[fd] - offset[fd]), buffer[fd] + offset[fd], read_bytes[fd] - offset[fd]);
-		offset[fd] = read_bytes[fd];
+		else
+		{
+			line = realloc(line, line_len + (bytes_read[fd] - offset[fd]));
+			if (!line)
+				return NULL;
+			memcpy(line + line_len, buffer[fd] + offset[fd], bytes_read[fd] - offset[fd]);
+			line_len += (bytes_read[fd] - offset[fd]);
+			offset[fd] = bytes_read[fd];
+		}
 	}
 
-	if (line_len > 0)
-	{
-		line = realloc(line, line_len + 1);
-		if (!line)
-		{
-			perror("Memory allocation failed");
-			return NULL;
-		}
-		line[line_len] = '\0';
-		line_count++;
-		return line;
-	}
-
-	return NULL;
+	line = realloc(line, line_len + 1);
+	if (!line)
+		return NULL;
+	line[line_len] = '\0';
+	line_count++;
+	return line;
 }
