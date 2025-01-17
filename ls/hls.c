bcfa490e4ece251a077f9b2fc7af1073d2b270ce
Natdog225 <Nathaniel.wilson@atlasschool.com>
2025-01-17 13:00:47 -0600

    Added a check to see if flags were used before printing everything.

--- ls/hls.c
+++ ls/hls.c
@@ -1,16 +1,28 @@
 #include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
  *
  * Return: Always 0
  */
-int main(void)
+int main(int argc, char *argv[])
 {
+	int show_hidden = 0;
 	DIR *dir;
 	struct dirent *entry;
+	int i;
 
+	for (i = 1; i < argc; i++)
+	{
+		if (strcmp(argv[i], "-a") == 0)
+		{
+			show_hidden = 1;
+		}
+	}
 	/* Open the current directory */
 	dir = opendir(".");
 	if (dir == NULL)
@@ -22,6 +34,10 @@ int main(void)
 	/* Read directory entries */
 	while ((entry = readdir(dir)) != NULL)
 	{
+		if (show_hidden == 0 && entry->d_name[0] == '.')
+		{
+			continue;
+		}
 		printf("%s  ", entry->d_name);
 	}
 	printf("\n");
