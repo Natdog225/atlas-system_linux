9c17148cc35014f3909550936a6da849abe55b66
Natdog225 <Nathaniel.wilson@atlasschool.com>
2025-01-17 14:40:04 -0600

    Made the print directory it's own function. And added some more checks

--- ls/hls.c
+++ ls/hls.c
@@ -1,9 +1,39 @@
 #include <dirent.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
 #include <string.h>
-#include <errno.h>
+#include <sys/stat.h>
+
+/**
+ * print_directory_contents - Prints the contents of a directory
+ * @dir_name: Name of the directory
+ * @show_hidden: Flag to show hidden files
+ */
+void print_directory_contents(char *dir_name, int show_hidden)
+{
+	DIR *dir;
+	struct dirent *entry;
+
+	dir = opendir(dir_name);
+	if (dir == NULL)
+	{
+		perror("opendir");
+		exit(1);
+	}
+
+	while ((entry = readdir(dir)) != NULL)
+	{
+		if (show_hidden == 0 && entry->d_name[0] == '.')
+		{
+			continue;
+		}
+		printf("%s  ", entry->d_name);
+	}
+	printf("\n");
+	closedir(dir);
+}
+
 /**
  * main - Entry point
  * @argc: Argument count
@@ -14,55 +44,70 @@
 int main(int argc, char *argv[])
 {
 	int show_hidden = 0;
-	DIR *dir;
-	struct dirent *entry;
-	char *path = ".";
 	struct stat path_stat;
 	int i;
 
 	for (i = 1; i < argc; i++)
 	{
-		if (argv[i][0] != '-')
+		if (argv[i][0] == '-')
 		{
-			path = argv[i];
-		}
-		else if (argv[i][0] == '-' && argv[i][1] == 'a' && argv[i][2] == '\0')
-		{
-			show_hidden = 1;
-		}
-	}
-
-	if (lstat(path, &path_stat) == -1)
-	{
-		fprintf(stderr, "%s: cannot access %s: %s\n",
-				argv[0], path, strerror(errno));
-		exit(1);
-	}
-
-	if (S_ISDIR(path_stat.st_mode))
-	{
-		dir = opendir(path);
-		if (dir == NULL)
-		{
-			perror("opendir");
-			exit(1);
-		}
-
-		while ((entry = readdir(dir)) != NULL)
-		{
-			if (show_hidden == 0 && entry->d_name[0] == '.')
+			if (argv[i][0] == '-' && argv[i][1] == 'a' && argv[i][2] == '\0')
 			{
-				continue;
+				show_hidden = 1;
 			}
-			printf("%s  ", entry->d_name);
 		}
-		printf("\n");
-		closedir(dir);
-	}
-	else
-	{
-		printf("%s\n", path);
+		else
+		{
+			/* Print directory name if multiple directories are provided */
+			if (argc > 2)
+			{
+				printf("%s:\n", argv[i]);
+			}
+
+			if (lstat(argv[i], &path_stat) == -1)
+			{
+				fprintf(stderr, "%s: cannot access %s: %s\n",
+						argv[0], argv[i], strerror(errno));
+				exit(1);
+			}
+
+			if (S_ISDIR(path_stat.st_mode))
+			{
+				print_directory_contents(argv[i], show_hidden);
+			}
+			else
+			{
+				printf("%s\n", argv[i]);
+			}
+		}
 	}
 
 	return (0);
+}
+if (S_ISDIR(path_stat.st_mode))
+{
+	dir = opendir(path);
+	if (dir == NULL)
+	{
+		perror("opendir");
+		exit(1);
+	}
+
+	while ((entry = readdir(dir)) != NULL)
+	{
+		if (show_hidden == 0 && entry->d_name[0] == '.')
+		{
+			continue;
+		}
+		printf("%s  ", entry->d_name);
+	}
+	printf("\n");
+	closedir(dir);
+}
+else
+{
+	printf("%s\n", path);
+}
+
+return (0);
 }
