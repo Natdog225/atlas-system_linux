7436103e08ee31a99e024b07a847aeb5d3f322fc
Natdog225 <Nathaniel.wilson@atlasschool.com>
2025-01-21 14:30:27 -0600

    Attempting to fix errors in formatting

--- ls/hls.c
+++ ls/hls.c
@@ -5,59 +5,49 @@
 #include <string.h>
 #include <sys/stat.h>
 
-/**
- * print_directory_contents - Prints the contents of a directory.
- * @dir_name: Name of the directory.
- * @show_hidden: Flag to show hidden files.
- * @one_per_line: Flag to print one entry per line.
- */
+#define MAX_FILES 1000
+
+int compare_strings(const void *a, const void *b)
+{
+	return strcmp(*(char **)a, *(char **)b);
+}
+
 void print_directory_contents(char *dir_name, int show_hidden, int one_per_line)
 {
 	DIR *dir;
 	struct dirent *entry;
+	char filenames[MAX_FILES][1024];
+	int count = 0;
+	int i;
 
 	dir = opendir(dir_name);
 	if (dir == NULL)
 	{
-		fprintf(stderr, "%s: cannot access '%s': %s\n",
-			"./hls_01", dir_name, strerror(errno));
+		perror("opendir");
 		exit(1);
 	}
 
 	while ((entry = readdir(dir)) != NULL)
 	{
 		if (show_hidden == 0 && entry->d_name[0] == '.')
-		{
-			continue;
-		}
 
-		printf("%s", entry->d_name);
+		strcpy(filenames[count], entry->d_name);
+		count++;
 
-		if (one_per_line)
-		{
-			printf("\n");
-		}
-		else
-		{
-			printf("  ");
-		}
-	}
-
-	if (!one_per_line)
-	{
-		printf("\n");
+		if (count >= MAX_FILES)
+			break;
 	}
 
 	closedir(dir);
+
+	qsort(filenames, count, sizeof(filenames[0]), compare_strings);
+
+	for (i = 0; i < count; i++)
+		printf("%-*s%s", 20, filenames[i], one_per_line ? "\n" : "  ");
+
+	printf("\n");
 }
 
-/**
- * main - Entry point
- * @argc: Argument count
- * @argv: Argument vector
- *
- * Return: Always 0
- */
 int main(int argc, char *argv[])
 {
 	int show_hidden = 0;
@@ -69,37 +59,37 @@ int main(int argc, char *argv[])
 	{
 		if (argv[i][0] == '-')
 		{
-			if (argv[i][0] == '-' && argv[i][1] == 'a' && argv[i][2] == '\0')
-			{
+			if (strcmp(argv[i], "-a") == 0)
 				show_hidden = 1;
-			}
-			else if (argv[i][0] == '-' && argv[i][1] == '1' && argv[i][2] == '\0')
-			{
+			else if (strcmp(argv[i], "-1") == 0)
 				one_per_line = 1;
+			else
+			{
+				fprintf(stderr, "%s: invalid option -- '%c'\n", argv[0], argv[i][1]);
+				exit(1);
 			}
 		}
 		else
-		{
-			if (lstat(argv[i], &path_stat) == -1)
-			{
-				fprintf(stderr, "%s: cannot access '%s': %s\n",
-					"./hls_01", argv[i], strerror(errno));
-				exit(1);
-			}
+			break;
+	}
 
-			if (S_ISDIR(path_stat.st_mode))
-			{
-				if (argc > 2)
-				{
-					printf("%s:\n", argv[i]);
-				}
-				print_directory_contents(argv[i], show_hidden, one_per_line);
-			}
-			else
-			{
-				printf("%s\n", argv[i]);
-			}
+
+	for (; i < argc; i++)
+	{
+		if (lstat(argv[i], &path_stat) == -1)
+		{
+			fprintf(stderr, "%s: cannot access %s: %s\n",
+				argv[0], argv[i], strerror(errno));
+			continue;
 		}
+
+		if (S_ISDIR(path_stat.st_mode))
+		{
+			printf("%s:\n", argv[i]);
+			print_directory_contents(argv[i], show_hidden, one_per_line);
+		}
+		else
+			printf("%s\n", argv[i]);
 	}
 
 	return (0);
