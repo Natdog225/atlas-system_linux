a2ff24481be916cf24537ad76b51a7374eb7f861
Natdog225 <Nathaniel.wilson@atlasschool.com>
2025-01-23 10:05:29 -0600

    Completely changed everything.

--- ls/hls.c
+++ ls/hls.c
@@ -1,89 +1,99 @@
 #include <dirent.h>
 #include <errno.h>
+#include <grp.h>
+#include <pwd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
-
-#define MAX_FILES 1000
+#include <time.h>
+#include <unistd.h>
+#include <limits.h>
 
 /**
- * custom_strcmp - Custom strcmp implementation
- * @s1: First string
- * @s2: Second string
- *
- * Return: Result of comparison
+ * print_long_format - Prints file information in long format
+ * @sb: Pointer to stat structure
+ * @name: Name of the file
  */
-int custom_strcmp(const char *s1, const char *s2)
+void print_long_format(struct stat *sb, const char *name)
 {
-	while (*s1 && *s2 && *s1 == *s2)
-	{
-		s1++;
-		s2++;
-	}
-	return (*s1 - *s2);
+	static char perms[] = "----------";
+	static const char *const fmt[] = {"---", "--x", "-w-", "-wx", "r--", "r-x", "rw-", "rwx"};
+	struct passwd *pw;
+	struct group *gr;
+	char date[32];
+
+	strftime(date, sizeof(date), "%b %d %H:%M", localtime(&sb->st_mtime));
+
+	perms[0] = (sb->st_mode & S_IFDIR) ? 'd' : '-';
+	perms[1] = fmt[(sb->st_mode >> 6) & 07][0];
+	perms[2] = fmt[(sb->st_mode >> 6) & 07][1];
+	perms[3] = fmt[(sb->st_mode >> 6) & 07][2];
+	perms[4] = fmt[(sb->st_mode >> 3) & 07][0];
+	perms[5] = fmt[(sb->st_mode >> 3) & 07][1];
+	perms[6] = fmt[(sb->st_mode >> 3) & 07][2];
+	perms[7] = fmt[sb->st_mode & 07][0];
+	perms[8] = fmt[sb->st_mode & 07][1];
+	perms[9] = fmt[sb->st_mode & 07][2];
+
+	pw = getpwuid(sb->st_uid);
+	gr = getgrgid(sb->st_gid);
+
+	printf("%s %lu %s %s %zu %s %s\n",
+		perms, sb->st_nlink,
+		pw ? pw->pw_name : "",
+		gr ? gr->gr_name : "",
+		sb->st_size, date, name);
 }
 
 /**
- * compare_strings - Helper function for qsort to compare strings
- * @a: First string
- * @b: Second string
- *
- * Return: Result of custom_strcmp
- */
-int compare_strings(const void *a, const void *b)
-{
-	return (custom_strcmp(*(char **)a, *(char **)b));
-}
-
-/**
- * print_directory_contents - Prints the contents of a directory
- * @dir_name: Name of the directory
+ * print_file_info - Prints file information
+ * @path: Path to the file
+ * @name: Name of the file
  * @show_hidden: Flag to show hidden files
- * @one_per_line: Flag to print one entry per line
  */
-void print_directory_contents(char *dir_name, int show_hidden, int one_per_line)
+void print_file_info(const char *path, const char *name, int show_hidden)
 {
 	DIR *dir;
 	struct dirent *entry;
-	char filenames[MAX_FILES][1024];
-	int count = 0;
-	int i;
+	struct stat sb;
+	char full_path[PATH_MAX];
 
-	dir = opendir(dir_name);
-	if (dir == NULL)
+	if (lstat(path, &sb) == -1)
 	{
-		perror("opendir");
+		fprintf(stderr, "./hls: cannot access '%s': %s\n", path, strerror(errno));
 		exit(1);
 	}
 
-	while ((entry = readdir(dir)) != NULL)
+	if (S_ISDIR(sb.st_mode))
 	{
-		if (show_hidden == 0 && entry->d_name[0] == '.')
+		dir = opendir(path);
+		if (dir == NULL)
 		{
-			continue;
+			perror("opendir");
+			exit(1);
 		}
-
-		strcpy(filenames[count], entry->d_name);
-		count++;
-		if (count >= MAX_FILES)
+		while ((entry = readdir(dir)) != NULL)
 		{
-			break;
+			if (!show_hidden && entry->d_name[0] == '.')
+			{
+				continue;
+			}
+			snprintf(full_path, sizeof(full_path), "%s/%s", path, entry->d_name);
+
+			if (lstat(full_path, &sb) == -1)
+			{
+				fprintf(stderr, "./hls: cannot access '%s': %s\n",
+					full_path, strerror(errno));
+				exit(1);
+			}
+			print_long_format(&sb, entry->d_name);
 		}
+		closedir(dir);
 	}
-
-	closedir(dir);
-
-	qsort(filenames, count, sizeof(filenames[0]), compare_strings);
-
-	for (i = 0; i < count; i++)
+	else
 	{
-		printf("%s%s", filenames[i], one_per_line ? "\n" : "  ");
-	}
-
-	if (!one_per_line)
-	{
-		printf("\n");
+		print_long_format(&sb, name);
 	}
 }
 
@@ -96,52 +106,36 @@ void print_directory_contents(char *dir_name, int show_hidden, int one_per_line)
  */
 int main(int argc, char *argv[])
 {
-	int show_hidden = 0;
-	int one_per_line = 0;
-	struct stat path_stat;
+	int show_all = 0;
+	int long_format = 0;
+	int opt;
 	int i;
 
-	for (i = 1; i < argc; i++)
+	while ((opt = getopt(argc, argv, "al")) != -1)
 	{
-		if (argv[i][0] == '-')
+		switch (opt)
 		{
-			if (custom_strcmp(argv[i], "-a") == 0)
-			{
-				show_hidden = 1;
-			}
-			else if (custom_strcmp(argv[i], "-1") == 0)
-			{
-				one_per_line = 1;
-			}
-			else
-			{
-				fprintf(stderr, "%s: invalid option -- '%c'\n", argv[0], argv[i][1]);
-				exit(1);
-			}
-		}
-		else
-		{
-			break; /* Stop parsing options when a non-option is encountered */
+		case 'a':
+			show_all = 1;
+			break;
+		case 'l':
+			long_format = 1;
+			break;
+		default:
+			fprintf(stderr, "Usage: %s [-al] [FILE...]\n", argv[0]);
+			exit(EXIT_FAILURE);
 		}
 	}
 
-	for (; i < argc; i++)
+	if (optind >= argc)
 	{
-		if (lstat(argv[i], &path_stat) == -1)
+		print_file_info(".", ".", show_all);
+	}
+	else
+	{
+		for (i = optind; i < argc; i++)
 		{
-			fprintf(stderr, "%s: cannot access %s: %s\n",
-				argv[0], argv[i], strerror(errno));
-			continue; /* Continue to the next argument if lstat fails */
-		}
-
-		if (S_ISDIR(path_stat.st_mode))
-		{
-			printf("%s:\n", argv[i]);
-			print_directory_contents(argv[i], show_hidden, one_per_line);
-		}
-		else
-		{
-			printf("%s\n", argv[i]);
+			print_file_info(argv[i], argv[i], show_all);
 		}
 	}
 
