39d7329659fcf1cb9f85a86fe786164d769ac082
Natdog225 <Nathaniel.wilson@atlasschool.com>
2025-02-25 14:09:26 -0600

    int change

--- readelf/elf_utils.c
+++ readelf/elf_utils.c
@@ -34,8 +34,7 @@ const char *get_osabi_string(unsigned char osabi, char *buffer, size_t buf_size)
 	case ELFOSABI_STANDALONE:
 		return ("Standalone App");
 	default:
-		/* Use the raw byte value directly */
-		snprintf(buffer, buf_size, "<unknown: %u>", osabi);
+		snprintf(buffer, buf_size, "<unknown: %x>", osabi);
 		return (buffer);
 	}
 }
