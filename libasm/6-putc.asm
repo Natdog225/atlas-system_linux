2a3f5def7a0a2841cd6a529052e7ff925fb32503
Natdog225 <Natethegreat225@gmail.com>
2025-03-06 21:23:26 +0000

    position dependant or something

--- libasm/6-putc.asm
+++ libasm/6-putc.asm
@@ -2,30 +2,28 @@
 ; size_t asm_putc(int c);
 
 section .data
-    char_buffer db 0   ; Single-byte buffer to hold the character
+    char_buffer db 0   ; Single-byte buffer
 
 section .text
     global asm_putc
 
 asm_putc:
-    ; --- Setup ---
-    push rbp          ; Save the base pointer
-    mov rbp, rsp     ; Set up a stack frame
+    push rbp
+    mov rbp, rsp
 
-    ; --- Prepare the system call arguments ---
-    mov byte [char_buffer], dil  ; Store the character (in dil) into the buffer
+    ; Load the character into the buffer (using a register)
+    mov byte [rel char_buffer wrt ..got], dil
 
-    mov rax, 1          ; System call number for write (sys_write)
-    mov rdi, 1          ; File descriptor 1 (stdout)
-    mov rsi, char_buffer ; Address of the character buffer
-    mov rdx, 1          ; Number of bytes to write (1 character)
+    ; Prepare the system call (write)
+    mov eax, 1          ; Syscall number for write
+    mov edi, 1          ; stdout file descriptor
+    lea rsi, [rel char_buffer wrt ..got]  ; Load the *effective* address of char_buffer
+    mov edx, 1          ; Number of bytes to write
 
-    ; --- Perform the system call ---
-    syscall             ; Execute the write system call
+    ; Perform the system call
+    syscall
 
-    ; --- Cleanup and Return ---
-    ; rax already contains the number of bytes written (or -1 on error)
-
-    mov rsp, rbp      ; Restore the stack pointer
-    pop rbp           ; Restore the base pointer
-    ret               ; Return
+    ; rax already contains the return value (bytes written)
+    mov rsp, rbp
+    pop rbp
+    ret
