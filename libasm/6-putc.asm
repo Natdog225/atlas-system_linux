c3432944304c0944670b7c13abf76b517e49c25f
Natdog225 <Natethegreat225@gmail.com>
2025-03-06 22:37:55 +0000

    Another attempt at fixing seg fault.

--- libasm/6-putc.asm
+++ libasm/6-putc.asm
@@ -1,39 +1,43 @@
 ; 6-putc.asm
+; size_t asm_putc(int c);
 
 section .data
     char_buffer db 0   ; Single-byte buffer
 
 section .text
     global asm_putc
-    global asm_write_char
+    global asm_write_char  ;write function globally accessible
 
+; This function performs the actual system call.
 asm_write_char:
-    push rbp         ; Save rbp
-    mov rbp, rsp     ; set up stack frame
-
+    push rbp              ; Preserve rbp
+    mov rbp, rsp          ; Set up stack Frame
     mov rax, 1          ; Syscall number for write
     mov rdi, 1          ; stdout file descriptor
-    mov rsi, [rbp + 16] ; Third argument is the buffer address
+    mov rsi, [rbp + 16] ; Load buffer address from the stack
     mov rdx, 1          ; Number of bytes to write (1)
     syscall             ; Perform the write
-
-    mov rsp, rbp     ; Restore the stack
-    pop rbp           ; Restore rbp
-
-    ret          ; Return
+    mov rsp, rbp       ; Restore Stack
+    pop rbp            ; Restore rbp
+    ret                 ; Return (to the C caller!)
 
 asm_putc:
-    push rbp           ; Save the caller's base pointer
-    mov  rbp, rsp      ; Set up our stack frame
-    sub  rsp, 8          ;create space for local and align the stack
+    push rbp           ; Save caller's base pointer.
+    mov rbp, rsp       ; set up stack frame
+    sub rsp, 8    ; Align stack to 16 bytes (and make space for char).
 
-    mov byte [rel char_buffer], dil  ; Store the character
+    mov byte [rel char_buffer], dil  ; Store the character (RIP-relative)
 
-    lea rdi, [rel char_buffer]   ; Get address of the buffer
-    push rdi                 ;put buffer address to stack
-    call asm_write_char         ;call to function
-    add rsp, 8        ;clean stack
+    ; Prepare for the jump to asm_write_char
+    lea rax, [rel char_buffer]   ; Get the address of the buffer
+    push rax                 ;push buffer to the stack.
 
-    mov rsp, rbp      ; Restore stack pointer
-    pop rbp           ; Restore base pointer
-    ret               ; Return to the C caller
+    ; Jump to the write function.
+    jmp asm_write_char      ; Jump
+
+    ; Execution continues *here*
+
+    add rsp, 8             ; Restore stack.
+    mov rsp, rbp           ; restore stack pointer
+    pop rbp            ; Restore base pointer.
+    ret               ; Return
