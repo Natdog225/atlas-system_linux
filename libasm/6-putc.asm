e4db084f8a8242b1cb00d06c0eaaa1e7e192670e
Natdog225 <Natethegreat225@gmail.com>
2025-03-06 21:27:22 +0000

    Maybe fix?

--- libasm/6-putc.asm
+++ libasm/6-putc.asm
@@ -6,22 +6,33 @@ section .data
 
 section .text
     global asm_putc
+    global asm_write_char  ; Make the write function globally accessible
+
+; This function performs the actual system call.
+asm_write_char:
+    mov rax, 1          ; Syscall number for write
+    mov rdi, 1          ; stdout file descriptor
+    mov rsi, rdi        ; Third argument (buffer) passed by the caller
+    mov rdx, 1          ; Number of bytes to write (1)
+    syscall             ; Perform the write
+    ret                 ;return the value of rax.
 
 asm_putc:
     push rbp
     mov rbp, rsp
 
-    ; Load the character into the buffer (using a register)
-    mov byte [rel char_buffer wrt ..got], dil
+    ; Load the character into the buffer (using RIP-relative addressing)
+    mov byte [rel char_buffer], dil
 
-    ; Prepare the system call (write)
-    mov eax, 1          ; Syscall number for write
-    mov edi, 1          ; stdout file descriptor
-    lea rsi, [rel char_buffer wrt ..got]  ; Load the *effective* address of char_buffer
-    mov edx, 1          ; Number of bytes to write
+    ; Prepare arguments for asm_write_char
+    ; We'll be jumping, not calling, so we set up the registers directly
+    lea rdi, [rel char_buffer] ;  Pass the buffer address to our helper function
 
-    ; Perform the system call
-    syscall
+    ; Jump to the write function
+    jmp asm_write_char        ; Jump to asm_write_char.
+
+    ; The code execution will continue at asm_write_char.
+    ; The return from asm_write_char will come back *here*.
 
     ; rax already contains the return value (bytes written)
     mov rsp, rbp
