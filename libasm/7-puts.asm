473b92871e78e21d948def04c4602b19df5089b1
Natdog225 <Natethegreat225@gmail.com>
2025-03-10 17:14:54 +0000

    Stack aint doing it

--- libasm/7-puts.asm
+++ libasm/7-puts.asm
@@ -6,28 +6,29 @@ asm_puts:
     push rbp
     mov rbp, rsp
     
-    ; Save only necessary registers
+    ; Save registers
     push rbx
     push r12
     push r13
 
     ; Get string length
-    mov r12, rdi        ; Save string pointer
-    call asm_strlen
+    mov r12, rdi        ; Save original string pointer
+    call asm_strlen     ; Returns length in rax
     mov r13, rax        ; Save string length
-
-    ; Add 1 for newline
-    inc r13             ; Total length including newline
 
     ; Setup syscall
     mov rax, 1          ; sys_write
     mov rdi, 1          ; stdout
     mov rsi, r12        ; string pointer
-    mov rdx, r13        ; length
+    mov rdx, r13        ; exact string length (no newline)
 
     ; Make the system call
     syscall
 
+    ; Return the string length (not length + 1)
+    mov rax, r13        ; Return original string length
+
+cleanup:
     ; Restore registers
     pop r13
     pop r12
