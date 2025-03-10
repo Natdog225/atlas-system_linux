9051af831b30e201b2723ea5fbf696bab163352f
Natdog225 <Natethegreat225@gmail.com>
2025-03-10 17:09:38 +0000

    Simplified it down and just trying to make the checker

--- libasm/0-strlen.asm
+++ libasm/0-strlen.asm
@@ -1,21 +1,36 @@
-; 0-strlen.asm
-; size_t asm_strlen(const char *str);
-
 section .text
-    global asm_strlen
+global asm_puts
+extern asm_strlen
 
-asm_strlen:
-    push rbp          ; Save caller's base pointer
-    mov rbp, rsp      ; Set up our own stack frame (optional, but good practice)
-    xor rax, rax      ; Initialize counter (rax) to 0
+asm_puts:
+    push rbp
+    mov rbp, rsp
+    
+    ; Save only necessary registers
+    push rbx
+    push r12
+    push r13
 
-.loop:
-    cmp byte [rdi + rax], 0  ; Compare the byte at (str + rax) with 0
-    je .end                  ; If it's 0 (end of string), jump to .end
-    inc rax                  ; Increment the counter
-    jmp .loop                ; Jump back to the beginning
+    ; Get string length
+    mov r12, rdi        ; Save string pointer
+    call asm_strlen
+    mov r13, rax        ; Save string length
 
-.end:
-    mov rsp, rbp    ; Restore stack pointer 
-    pop rbp          ; Restore caller's base pointer
-    ret              ; Return
+    ; Add 1 for newline
+    inc r13             ; Total length including newline
+
+    ; Setup syscall
+    mov rax, 1          ; sys_write
+    mov rdi, 1          ; stdout
+    mov rsi, r12        ; string pointer
+    mov rdx, r13        ; length
+
+    ; Make the system call
+    syscall
+
+    ; Restore registers
+    pop r13
+    pop r12
+    pop rbx
+    pop rbp
+    ret
