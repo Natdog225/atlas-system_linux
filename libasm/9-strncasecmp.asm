f184ba25a2c5124b54bfa9d0ececd00ea7ceaccb
Natdog225 <Natethegreat225@gmail.com>
2025-03-10 20:54:48 +0000

    Betty fixes

--- libasm/9-strncasecmp.asm
+++ libasm/9-strncasecmp.asm
@@ -5,29 +5,29 @@ asm_strncasecmp:
     push rbp
     mov rbp, rsp
     push rbx        ; Must preserve rbx
-    
+
     mov rcx, rdx    ; rcx = n (counter)
-    
+
 .loop:
     test rcx, rcx    ; Check if n == 0
     jz .equal       ; If n == 0, strings are equal up to n
-    
+
     mov al, byte [rdi]  ; Load byte from s1
     mov bl, byte [rsi]  ; Load byte from s2
-    
+
     ; If it hits null in either string, they're not equal
     test al, al
     jz .null_s1
     test bl, bl
     jz .null_s2
-    
+
     ; Convert al to lowercase if needed
     cmp al, 'A'
     jl .check_bl
     cmp al, 'Z'
     jg .check_bl
     add al, 32
-    
+
 .check_bl:
     ; Convert bl to lowercase if needed
     cmp bl, 'A'
@@ -35,11 +35,11 @@ asm_strncasecmp:
     cmp bl, 'Z'
     jg .compare
     add bl, 32
-    
+
 .compare:
     cmp al, bl      ; Compare the bytes
     jne .diff       ; return difference
-    
+
     inc rdi         ; Move to next character in s1
     inc rsi         ; Move to next character in s2
     dec rcx         ; Decrement counter
@@ -69,7 +69,7 @@ asm_strncasecmp:
 
 .equal:
     xor eax, eax    ; Return 0 yay equality!
-    
+
 .done:
     ; Restore saved registers
     pop rbx
