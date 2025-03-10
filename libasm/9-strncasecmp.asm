60e92329e248d5d1ac18b3fd1e08d3a611fbdd66
Natdog225 <Natethegreat225@gmail.com>
2025-03-10 20:34:03 +0000

    Slight fix

--- libasm/9-strncasecmp.asm
+++ libasm/9-strncasecmp.asm
@@ -1,10 +1,10 @@
 section .text
 global asm_strncasecmp
 asm_strncasecmp:
-    ; Save registers
+    ; Save registers that must be preserved according to x86_64 System V AMD64 ABI
     push rbp
     mov rbp, rsp
-    push rbx        ;preserve rbx
+    push rbx        ; Must preserve rbx
     
     mov rcx, rdx    ; rcx = n (counter)
     
@@ -31,8 +31,8 @@ asm_strncasecmp:
     add bl, 32      ; Convert to lowercase
     
 .compare:
-    cmp al, bl      ; Compare bytes
-    jne .diff       ; If not equal, return difference, ew
+    cmp al, bl      ; Compare the bytes
+    jne .diff       ; If not equal, return difference
     
     ; Check for null terminator in either string
     test al, al     ; Check if we hit end of s1
@@ -40,10 +40,10 @@ asm_strncasecmp:
     test bl, bl     ; Check if we hit end of s2
     jz .equal       ; If null, strings are equal up to this point
     
-    inc rdi         ; Move to next char in s1
-    inc rsi         ; Move to next char in s2
-    dec rcx         ; Decrement
-    jmp .loop       ; Continue
+    inc rdi         ; Move to next character in s1
+    inc rsi         ; Move to next character in s2
+    dec rcx         ; Decrement counter
+    jmp .loop       ; Continue loop
 
 .diff:
     ; Return the difference (al - bl)
