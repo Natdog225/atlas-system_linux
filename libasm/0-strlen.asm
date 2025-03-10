cc6b827cee596511efbd491460988f46e8e8e78c
Natdog225 <Natethegreat225@gmail.com>
2025-03-10 16:38:22 +0000

    Adjusted strlen for a sec just to see if that was the problem, it was not.

--- libasm/0-strlen.asm
+++ libasm/0-strlen.asm
@@ -5,13 +5,17 @@ section .text
     global asm_strlen
 
 asm_strlen:
-    xor rax, rax        ; Initialize counter (rax) to 0
+    push rbp          ; Save caller's base pointer
+    mov rbp, rsp      ; Set up our own stack frame (optional, but good practice)
+    xor rax, rax      ; Initialize counter (rax) to 0
 
 .loop:
     cmp byte [rdi + rax], 0  ; Compare the byte at (str + rax) with 0
-    je .end             ; If it's 0 (end of string), jump to .end
-    inc rax             ; Increment the counter
-    jmp .loop           ; Jump back to the beginning of the loop
+    je .end                  ; If it's 0 (end of string), jump to .end
+    inc rax                  ; Increment the counter
+    jmp .loop                ; Jump back to the beginning
 
 .end:
-    ret                 ; Return (rax contains the length)
+    mov rsp, rbp    ; Restore stack pointer 
+    pop rbp          ; Restore caller's base pointer
+    ret              ; Return
