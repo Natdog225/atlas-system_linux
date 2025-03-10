cdfaf1d131a476588e75f87c5964b269cdca00db
Natdog225 <Natethegreat225@gmail.com>
2025-03-10 20:56:29 +0000

    Updated comments

--- libasm/9-strncasecmp.asm
+++ libasm/9-strncasecmp.asm
@@ -1,19 +1,19 @@
 section .text
 global asm_strncasecmp
 asm_strncasecmp:
-    ; Save registers that must be preserved according to x86_64 System V AMD64 ABI
+    ; Save registers
     push rbp
     mov rbp, rsp
-    push rbx        ; Must preserve rbx
+    push rbx
 
     mov rcx, rdx    ; rcx = n (counter)
 
 .loop:
     test rcx, rcx    ; Check if n == 0
-    jz .equal       ; If n == 0, strings are equal up to n
+    jz .equal       ; If n == 0, equality up to n
 
-    mov al, byte [rdi]  ; Load byte from s1
-    mov bl, byte [rsi]  ; Load byte from s2
+    mov al, byte [rdi]  ; Load byte s1
+    mov bl, byte [rsi]  ; Load byte s2
 
     ; If it hits null in either string, they're not equal
     test al, al
@@ -49,15 +49,15 @@ asm_strncasecmp:
     ; s1 is null, s2 has more characters
     mov al, 0
     mov bl, byte [rsi]
-    sub al, bl      ; Will be negative
+    sub al, bl      ; Will be negative...I think?
     movsx eax, al
     jmp .done
 
 .null_s2:
-    ; s2 is null, s1 has more characters
+    ; s2 is null, s1 has more characters, player 1 is always better
     mov al, byte [rdi]
     mov bl, 0
-    sub al, bl      ; Will be positive
+    sub al, bl      ; Will be positive...Right?
     movsx eax, al
     jmp .done
 
