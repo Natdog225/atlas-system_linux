f416ee099f044256c808e62add33a922d759d660
Natdog225 <Natethegreat225@gmail.com>
2025-03-11 17:38:50 +0000

    Attempting to make checker happy

--- libasm/10-strspn.asm
+++ libasm/10-strspn.asm
@@ -1,42 +1,52 @@
+; asm_strspn.asm
+; size_t asm_strspn(const char *s, const char *accept);
+
 section .text
-global asm_strspn
+    global asm_strspn
+
 asm_strspn:
-    ; Save registers
     push rbp
     mov rbp, rsp
-    push rbx
-    xor rax, rax    ; Initialize return value to 0
+    ; Use rcx as the counter
+    xor rcx, rcx  ; Initialize the count (rcx) to 0
 
-.loop:
-    mov al, byte [rdi]  ; Load character from s
-    test al, al        ; Check for null terminator
-    jz .done          ; If null, we're done
+.outer_loop:
+    mov al, byte [rdi + rcx] ; Load char from 's'
+    test al, al          ; Check for null terminator in 's'
+    jz .done             ; If null terminator, return the count
 
-    ; Scan accept string to find match
-    mov rsi, rdx      ; Reset rsi to start of accept string
-.scan_accept:
-    mov bl, byte [rsi] ; Load character from accept
-    test bl, bl       ; Check for null terminator
-    jz .next_char    ; If null,no match, bit sad innit 
-    
-    cmp al, bl       ; Compare characters
-    je .found_match  ; If equal, we found a match
-    
-    inc rsi          ; Move to next character in accept
-    jmp .scan_accept ; Continue
+    push rdi  ; Save rdi.
+    push rcx  ; Save rcx.
+
+    ; Inner loop: Scan 'accept' string for a match
+    mov rdi, rsi        ; rdi = accept string (inner loop)
+
+.inner_loop:
+    mov bl, byte [rdi]  ; Load char from 'accept'
+    test bl, bl          ; Check for null terminator in 'accept'
+    jz .not_found       ; If null terminator, char not found
+
+    cmp al, bl           ; Compare s[i] with accept[j]
+    je .found_match     ; If match, increment count and continue outer loop
+
+    inc rdi             ; Move to next char in 'accept'
+    jmp .inner_loop     ; Check next char in accept
 
 .found_match:
-    inc rax          ; Increment match
-    inc rdi          ; Move to next character
-    jmp .loop        ; Continue
+    ; Character found in accept string.
+    inc rcx           ; Increment
+    pop rcx         ; Restore
+    pop rdi
+    jmp .outer_loop  ; continue next character in 's'
 
-.next_char:
-    xor rax, rax    ; Reset count to 0
-    jmp .done       ; Exit with 0
+.not_found:
+    ; Character not found in accept string.
+    pop rcx ;restore registers.
+    pop rdi
+    ; Fall through to .done
 
 .done:
-    ; Restore saved registers
-    pop rbx
+     mov rax, rcx    ; The count is the return, put it on rax.
     mov rsp, rbp
     pop rbp
     ret
