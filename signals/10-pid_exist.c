b9ed625c5ad609147c0c45be16acd00c99a1c554
Natdog225 <Nathaniel.wilson@atlasschool.com>
2025-03-22 16:28:03 -0500

    betty

--- signals/10-pid_exist.c
+++ signals/10-pid_exist.c
@@ -1,6 +1,11 @@
 #include <signal.h>
 #include <errno.h>
-
+/**
+ * pid_exist - tests if a process exists, given its PID
+ * @pid: process id
+ *
+ * Return: 1 if process exists, 0 if not
+ */
 int pid_exist(pid_t pid)
 {
 	return (kill(pid, 0) == 0 || errno != ESRCH);
