db695ff017f452a2157bb008345d273a985d875d
Natdog225 <Nathaniel.wilson@atlasschool.com>
2025-03-22 15:38:52 -0500

    betty stuff

--- signals/1-current_handler_signal.c
+++ signals/1-current_handler_signal.c
@@ -1,12 +1,15 @@
 #include <signal.h>
-
+/**
+ * current_handler_signal - retrieves the current handler of the signal SIGINT
+ *
+ * Return: void pointer of __sighandler_t
+ */
 void (*current_handler_signal(void))(int)
 {
 	void (*handler)(int);
 
-	// signal(SIGINT, SIG_DFL) gets the current handler then temp sets to defualt
 	handler = signal(SIGINT, SIG_DFL);
 	signal(SIGINT, handler);
 
-	return handler;
+	return (handler);
 }
