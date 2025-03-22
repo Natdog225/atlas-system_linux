ea6f750a318e3df38cab5eabeab662d43f3381db
Natdog225 <Nathaniel.wilson@atlasschool.com>
2025-03-22 15:50:50 -0500

    Betty

--- signals/2-handle_sigaction.c
+++ signals/2-handle_sigaction.c
@@ -1,24 +1,35 @@
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
-
+/**
+ * sigint_handler - handler for SIGINTs
+ * @sig: signal number
+ *
+ * Return: void
+ */
 void handle_sigint(int signum)
 {
 	printf("Gotcha! [%d]\n", signum);
+	fflush(stdout);
 }
-
+/**
+ * sigint_handler - handler for SIGINTs
+ * @sig: signal number
+ *
+ * Return: void
+ */
 int handle_sigaction(void)
 {
 	struct sigaction sa;
 
 	sa.sa_handler = handle_sigint;
-	sigemptyset(&sa.sa_mask); // Block no other signals during handling
+	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = 0;
 
 	if (sigaction(SIGINT, &sa, NULL) == -1)
 	{
 		perror("sigaction");
-		return -1;
+		return (-1);
 	}
-	return 0;
+	return (0);
 }
