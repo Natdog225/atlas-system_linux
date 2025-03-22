91f75cd08c84ba2bbc07f68aa70060c45881b774
Natdog225 <Nathaniel.wilson@atlasschool.com>
2025-03-22 16:16:39 -0500

    betty for 7

--- signals/7-signal_send.c
+++ signals/7-signal_send.c
@@ -1,23 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <signal.h>
-#include <errno.h>
+#include "signals.h"
 
+/**
+ * main - Sends the SIGINT signal to a process.
+ * @argc: The number of command-line args
+ * @argv: An array of command-line arg strings.
+ *
+ * Return: EXIT_SUCCESS when it succeeds failure when it doesn't.
+ */
 int main(int argc, char *argv[])
 {
+	pid_t pid;
+
 	if (argc != 2)
 	{
 		printf("Usage: %s <pid>\n", argv[0]);
-		return EXIT_FAILURE;
+		return (EXIT_FAILURE);
 	}
 
-	pid_t pid = atoi(argv[1]); // Convert PID string to integer
+	pid = atoi(argv[1]); /* Convert PID string to integer */
 
 	if (kill(pid, SIGINT) == -1)
 	{
 		perror("kill");
-		return EXIT_FAILURE;
+		return (EXIT_FAILURE);
 	}
 
-	return EXIT_SUCCESS;
+	return (EXIT_SUCCESS);
 }
