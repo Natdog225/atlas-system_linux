c0bdd97ca005d3f0b9b2d09a5503830f24e57195
Natdog225 <Nathaniel.wilson@atlasschool.com>
2025-03-31 14:37:25 -0500

    Number 3

---
+++ 0x08_CPython/3-python.c
@@ -0,0 +1,99 @@
+#include <Python.h>
+#include <stdio.h>
+
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+void print_python_list(PyObject *p);
+
+void print_python_float(PyObject *p)
+{
+	PyFloatObject *float_obj;
+	double value;
+
+	printf("[.] float object info\n")
+	if (!PyFloat_Check(p))
+	{
+		printf("  [ERROR] Invalid Float Object\n");
+		fflush(stdout);
+		return;
+	}
+
+	float_obj = (PyFloatObject *)p;
+
+	value = float_obj->ob_fval;
+
+	printf("  value: %g\n", value);
+	fflush(stdout);
+
+}
+
+void print_python_bytes(PyObject *p)
+{
+	PyBytesObject *bytes_obj;
+	Py_ssize_t size, i, limit;
+	char *data;
+
+	printf("[.] bytes object info\n");
+	/* Check if it's valid PyObject */
+	if (!PyBytes_Check(p))
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		fflush(stdout);
+		return;
+	}
+	bytes_obj = (PyBytesObject *)p;
+	size = ((PyVarObject *)p)->ob_size;
+	data = bytes_obj->ob_sval;
+
+	printf("  size: %zd\n", size);
+
+	printf("  trying string: %s\n", data);
+
+	limit = size + 1;
+	if (limit > 10)
+	{
+		limit = 10;
+	}
+
+	printf("  first %zd bytes:", limit);
+	for (i = 0; i < limit; i++)
+	{
+		printf(" %02x", (unsigned char)data[i]);
+	}
+	printf("\n");
+	fflush(stdout);
+}
+
+void print_python_list(PyObject *p)
+{
+	PyListObject *list = (PyListObject *)p;
+	Py_ssize_t size, allocated, i;
+	const char *typeName;
+
+	size = ((PyVarObject *)p)->ob_size;
+	allocated = list->allocated;
+
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %zd\n", size);
+	printf("[*] Allocated = %zd\n", allocated);
+
+	for (i = 0; i < size; ++i)
+	{
+		PyObject *element = list->ob_item[i];
+
+		typeName = element->ob_type->tp_name;
+
+		printf("Element %zd: %s\n", i, typeName);
+
+		if (PyBytes_Check(element))
+		{
+			print_python_bytes(element);
+		}
+		else if (PyFloat_Check(element))
+		{
+			print_python_float(element)
+		}
+
+	}
+	fflush(stdout);
+}
